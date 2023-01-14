06493f16f94eebca436010f560d6802bef4831c9
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2023-01-14 11:22:19 -0800

    auto balance tuned

--- src/main/java/frc/robot/IDs.java
+++ src/main/java/frc/robot/IDs.java
@@ -3,32 +3,32 @@ package frc.robot;
 import edu.wpi.first.wpilibj.I2C.Port;
 
 public final class IDs {
-    //ControllerPorts
-    public static final int CONTROLLER_DRIVE_PORT = 0;
-    public static final int CONTROLLER_OPERATOR_PORT = 1;
+  // ControllerPorts
+  public static final int CONTROLLER_DRIVE_PORT = 0;
+  public static final int CONTROLLER_OPERATOR_PORT = 1;
 
-    // Drive
-    public static final int DRIVE_LEFT_LEAD_DEVICE = 1;
-    public static final int DRIVE_LEFT_FOLLOW_DEVICE = 2;
-    public static final int DRIVE_RIGHT_LEAD_DEVICE = 3;
-    public static final int DRIVE_RIGHT_FOLLOW_DEVICE = 4;
+  // Drive
+  public static final int DRIVE_LEFT_LEAD_DEVICE = 1;
+  public static final int DRIVE_LEFT_FOLLOW_DEVICE = 2;
+  public static final int DRIVE_RIGHT_LEAD_DEVICE = 3;
+  public static final int DRIVE_RIGHT_FOLLOW_DEVICE = 4;
 
-  //Intake
-    public static final int INTAKE_LEAD_DEVICE = 5;
-    
-    // Lift
-    public static final int LIFT_UPPER_SWITCH = 9;
-    public static final int LIFT_LOWER_SWITCH = 8;
-    public static final int LIFT_WINCH_DEVICE0 = 8;
-    public static final int LIFT_WINCH_DEVICE1 = 9;
+  // Intake
+  public static final int INTAKE_LEAD_DEVICE = 5;
 
-    //NavX Gyro
-    public static final Port AHRS_PORT_ID = Port.kMXP;
+  // Lift
+  public static final int LIFT_UPPER_SWITCH = 9;
+  public static final int LIFT_LOWER_SWITCH = 8;
+  public static final int LIFT_WINCH_DEVICE0 = 8;
+  public static final int LIFT_WINCH_DEVICE1 = 9;
 
-    //Beam
-    public static final double BEAM_BALANACED_DRIVE_KP = 0.015; // P (Proportional) constant of a PID loop
-    public static final double BEAM_BALANCED_GOAL_DEGREES = 0;
-    public static final double BEAM_BALANCED_ANGLE_THRESHOLD_DEGREES = 1;
-    public static final double BACKWARDS_BALANCING_EXTRA_POWER_MULTIPLIER = 1.35;
-    
+  // NavX Gyro
+  public static final Port AHRS_PORT_ID = Port.kMXP;
+
+  // Beam
+  public static final double BEAM_BALANACED_DRIVE_KP = 0.005; //.015, lower = slower, as weight goes up number needs to go up
+  public static final double BEAM_BALANCED_GOAL_DEGREES = 0;
+  public static final double BEAM_BALANCED_ANGLE_THRESHOLD_DEGREES = 1;
+  public static final double BACKWARDS_BALANCING_EXTRA_POWER_MULTIPLIER = 1.35;
+
 }
